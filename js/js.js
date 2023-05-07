0e6665d6d00cc1736351d8ed2f65cc8ea6690aad
salogelTorres <luistorres@eletorres.com>
2023-05-07 14:28:14 +0200

    Refactor

--- js/js.js
+++ js/js.js
@@ -1,26 +1,12 @@
 navigator.requestMIDIAccess().then(function (midiAccess) {
     var inputs = midiAccess.inputs.values();
-    console.log(inputs)
     for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
         input.value.onmidimessage = function (event) {
             if (event.data[0] === 144) { // Note on message
                 var note = event.data[1];
                 var velocity = event.data[2];
-                if (velocity != 0) {
-                    let color = (1 - (velocity / 127)) * 255
-                    console.log(color)
-                    document.getElementById(note).style.backgroundColor = `rgb(255,${color},${color})`
+                pressTheKey(note, velocity)
 
-                }
-                else {
-                    if (document.getElementById(note).className == "black-key")
-                        document.getElementById(note).style.backgroundColor = 'black'
-                    else
-                        document.getElementById(note).style.backgroundColor = 'white'
-
-                }
-
-                console.log("Note " + note + " on, velocity " + velocity);
             } else if (event.data[0] === 128) { // Note off message
                 var note = event.data[1];
                 var velocity = event.data[2];
@@ -29,3 +15,20 @@ navigator.requestMIDIAccess().then(function (midiAccess) {
         }
     }
 });
+
+
+function pressTheKey(keyNumber, velocity) {
+    let keyUI = document.getElementById(keyNumber);
+    if (velocity != 0) {
+        let color = (1 - (velocity / 127)) * 255
+        keyUI.style.backgroundColor = `rgb(255,${color},${color})`
+
+    }
+    else {
+        if (keyUI.className == "black-key")
+            keyUI.style.backgroundColor = 'black'
+        else
+            keyUI.style.backgroundColor = 'white'
+
+    }
+}
