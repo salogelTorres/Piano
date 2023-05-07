3e27b31845e47add91064ec1318ad9fb0a583436
RocioChaves <rociochaher@gmail.com>
2023-05-07 23:48:51 +0200

    Refactor: separamos implementación del synth. También reproducimos audio al hacer click en las teclas

--- js/js.js
+++ js/js.js
@@ -1,60 +1,54 @@
-var context = new AudioContext(),
-    oscillators = {};
+import { Synth } from "./synth.js";
 
+listenKeyboardClick();
+listenMidiDevicePlay();
+let synth = new Synth();
 
-
-navigator.requestMIDIAccess().then(function (midiAccess) {
+function listenMidiDevicePlay() {
+  navigator.requestMIDIAccess().then(function (midiAccess) {
     var inputs = midiAccess.inputs.values();
-    for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
-        input.value.onmidimessage = function (event) {
-            if (event.data[0] === 144) { // Note on message
-                var note = event.data[1];
-                var velocity = event.data[2];
-                pressTheKey(note, velocity)
-
-            } else if (event.data[0] === 128) { // Note off message
-                var note = event.data[1];
-                var velocity = event.data[2];
-                console.log("Note " + note + " off, velocity " + velocity);
-            }
+    for (
+      var input = inputs.next();
+      input && !input.done;
+      input = inputs.next()
+    ) {
+      input.value.onmidimessage = function (event) {
+        if (event.data[0] === 144) {
+          // Note on message
+          var note = event.data[1];
+          var velocity = event.data[2];
+          pressTheKey(note, velocity);
+        } else if (event.data[0] === 128) {
+          // Note off message
+          var note = event.data[1];
+          var velocity = event.data[2];
         }
+      };
     }
-});
-
+  });
+}
 
 function pressTheKey(keyNumber, velocity) {
-    let keyUI = document.getElementById(keyNumber);
-    let frequency = midiNoteToFrequency(keyNumber);
-    if (velocity != 0) {
-        let color = (1 - (velocity / 127)) * 255
-        keyUI.style.backgroundColor = `rgb(255,${color},${color})`
-        playNote(frequency);
+  let keyUI = document.getElementById(keyNumber);
+  if (velocity != 0) {
+    let color = (1 - velocity / 127) * 255;
+    keyUI.style.backgroundColor = `rgb(255,${color},${color})`;
+    synth.playNote(keyNumber);
+  } else {
+    if (keyUI.className == "black-key") keyUI.style.backgroundColor = "black";
+    else keyUI.style.backgroundColor = "white";
+    synth.stopNote(keyNumber);
+  }
+}
+
+function listenKeyboardClick() {
+  document.querySelector(".keys").addEventListener("click", function (e) {
+    if (e.target.id) {
+      let note = e.target.id;
+      pressTheKey(note, 50);
+      setTimeout(function () {
+        pressTheKey(note, 0);
+      }, 100);
     }
-    else {
-        if (keyUI.className == "black-key")
-            keyUI.style.backgroundColor = 'black'
-        else
-            keyUI.style.backgroundColor = 'white'
-        stopNote(frequency);
-    }
-}
-
-
-
-function midiNoteToFrequency(note) {
-    return Math.pow(2, ((note - 69) / 12)) * 440
-}
-
-
-function playNote(frequency) {
-    oscillators[frequency] = context.createOscillator();
-    oscillators[frequency].frequency.value = frequency;
-    oscillators[frequency].connect(context.destination);
-    oscillators[frequency].start(context.currentTime);
-}
-
-
-function stopNote(frequency) {
-    oscillators[frequency].stop(context.currentTime);
-    oscillators[frequency].disconnect();
+  });
 }
---
+++ js/synth.js
@@ -0,0 +1,26 @@
+export class Synth {
+  #context = null;
+  #oscillators = {}
+
+  constructor() {
+    (this.#context = new AudioContext()), (this.#oscillators = {});
+  }
+
+  #midiNoteToFrequency(note) {
+    return Math.pow(2, (note - 69) / 12) * 440;
+  }
+
+  playNote(note) {
+    let frequency = this.#midiNoteToFrequency(note);
+    this.#oscillators[frequency] = this.#context.createOscillator();
+    this.#oscillators[frequency].frequency.value = frequency;
+    this.#oscillators[frequency].connect(this.#context.destination);
+    this.#oscillators[frequency].start(this.#context.currentTime);
+  }
+
+  stopNote(note) {
+    let frequency = this.#midiNoteToFrequency(note);
+    this.#oscillators[frequency].stop(this.#context.currentTime);
+    this.#oscillators[frequency].disconnect();
+  }
+}
