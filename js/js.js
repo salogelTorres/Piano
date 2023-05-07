af5f387b02b3f828f808a3d06a980fb19285456c
salogelTorres <luistorres@eletorres.com>
2023-05-07 17:04:11 +0200

    Añadimos un sintetizador

--- js/js.js
+++ js/js.js
@@ -1,3 +1,8 @@
+var context = new AudioContext(),
+    oscillators = {};
+
+
+
 navigator.requestMIDIAccess().then(function (midiAccess) {
     var inputs = midiAccess.inputs.values();
     for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
@@ -19,16 +24,37 @@ navigator.requestMIDIAccess().then(function (midiAccess) {
 
 function pressTheKey(keyNumber, velocity) {
     let keyUI = document.getElementById(keyNumber);
+    let frequency = midiNoteToFrequency(keyNumber);
     if (velocity != 0) {
         let color = (1 - (velocity / 127)) * 255
         keyUI.style.backgroundColor = `rgb(255,${color},${color})`
-
+        playNote(frequency);
     }
     else {
         if (keyUI.className == "black-key")
             keyUI.style.backgroundColor = 'black'
         else
             keyUI.style.backgroundColor = 'white'
-
+        stopNote(frequency);
     }
+}
+
+
+
+function midiNoteToFrequency(note) {
+    return Math.pow(2, ((note - 69) / 12)) * 440
+}
+
+
+function playNote(frequency) {
+    oscillators[frequency] = context.createOscillator();
+    oscillators[frequency].frequency.value = frequency;
+    oscillators[frequency].connect(context.destination);
+    oscillators[frequency].start(context.currentTime);
+}
+
+
+function stopNote(frequency) {
+    oscillators[frequency].stop(context.currentTime);
+    oscillators[frequency].disconnect();
 }
